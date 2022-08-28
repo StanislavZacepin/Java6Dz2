9aa66be31b4367bcf5ee94826431706472f284c6
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-08-28 18:59:36 +0300

    Подготовка

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        boolean isFormatted = false;
-        String request = "пожалуйста форматируйте код";
-        // исправили теперь build succcessful
-        if (isFormatted) {
-        }
-    }
-}
---
+++ src/main/java/UndergroundSystem.java
@@ -0,0 +1,20 @@
+class UndergroundSystem {
+    public UndergroundSystem() {
+
+    }
+    public static void main(String[] args) {
+
+    }
+    public void checkIn(int id, String stationName, int t) {
+
+    }
+
+    public void checkOut(int id, String stationName, int t) {
+
+    }
+
+    public double getAverageTime(String startStation, String endStation) {
+
+    }
+}
+
